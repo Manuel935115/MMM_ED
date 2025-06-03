429d5df754122572094b799b665cfb1cf6892eef
Pablo Morato <pablo.morato@ceu.es>
2025-06-03 19:07:19 +0200

    MMM_Commit de la primera clase coche

---
+++ src/examen/MMM_Coch.java
@@ -0,0 +1,62 @@
+package examen;
+
+public class MMM_Coch {
+
+	String Matricula;
+	public String getMatricula() {
+		return Matricula;
+	}
+	public void setMatricula(String matricula) {
+		Matricula = matricula;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getNumeroPlazas() {
+		return NumeroPlazas;
+	}
+	public void setNumeroPlazas(int numeroPlazas) {
+		NumeroPlazas = numeroPlazas;
+	}
+	public double getVelocidadMedia() {
+		return VelocidadMedia;
+	}
+	public void setVelocidadMedia(double velocidadMedia) {
+		VelocidadMedia = velocidadMedia;
+	}
+	public String getTipoCombustible() {
+		return TipoCombustible;
+	}
+	public void setTipoCombustible(String tipoCombustible) {
+		TipoCombustible = tipoCombustible;
+	}
+	public Double getConsumo() {
+		return consumo;
+	}
+	public void setConsumo(Double consumo) {
+		this.consumo = consumo;
+	}
+	
+	public MMM_Cche(String matricula, String modelo, int numeroPlazas, double velocidadMedia, String tipoCombustible,
+			Double consumo) {
+		super();
+		Matricula = matricula;
+		this.modelo = modelo;
+		NumeroPlazas = numeroPlazas;
+		VelocidadMedia = velocidadMedia;
+		TipoCombustible = tipoCombustible;
+		this.consumo = consumo;
+	}
+	int NumeroPlazas;
+	 public MMM_Cche() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	double VelocidadMedia;
+	 String TipoCombustible;
+	 Double consumo;
+	 String modelo;
+}
